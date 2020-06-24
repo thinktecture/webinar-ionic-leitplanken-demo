c861607b32474fee22af55ab462c079c7aff50f2
Max Schulte <max.schulte@thinktecture.com>
2020-06-24 08:09:42 +0200

    add comments to camera compoent

--- src/app/components/camera/camera.component.ts
+++ src/app/components/camera/camera.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { ToastController } from '@ionic/angular';
-import { Plugins, CameraResultType } from '@capacitor/core';
-const { Camera } = Plugins;
+// import the correct camera from capacitor
+import { CameraResultType } from '@capacitor/core';
 
+// A good example how to build an app and use the camera 
+// https://ionicframework.com/docs/angular/your-first-app
 @Component({
     selector: 'app-camera',
     templateUrl: './camera.component.html',
@@ -16,18 +18,23 @@ export class CameraComponent {
     }
 
     initPictureProcess() {
+        // fake click on photo input to open the dialog
         this.photoInput.nativeElement.click();
     }
 
     async handleInput() {
+        // get selected files
         const files: FileList = this.photoInput.nativeElement.files;
         if (!files && files.length <= 0) {
             return;
         }
         try {
+            // only convert the first selected photo to base64 and emit
             const base64Photo = await this.convertBase64(files[ 0 ]);
             this.capture.emit(base64Photo);
         } catch {
+
+            // notify user on error
             const toast = await this.toasts.create({
                 message: 'Could not take picture',
                 duration: 2000,
@@ -52,17 +59,30 @@ export class CameraComponent {
     }
 
     async takePictureCapacitor() {
+        // 1. try erros, e.g. the user denied a permission
         try {
+
+            // 2. get pohoto with quality and specific result type
             const image = await Camera.getPhoto({
                 quality: 90,
                 allowEditing: true,
                 resultType: CameraResultType.Base64,
             });
+
+            // 3. make the base64 encoded date as url accessibile and emit the new photo
             const base64Image = image.base64String;
             this.capture.emit('data:image/jpeg;base64, ' + base64Image);
 
         } catch (e) {
+            // notify user on error
             console.error(e);
+            const toast = await this.toasts.create({
+                message: 'Could not take picture',
+                duration: 2000,
+                color: 'warning',
+                translucent: true,
+            });
+            toast.present();
         }
     }
 }
